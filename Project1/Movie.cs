9dab3704234709292cde0cec1aad6fbdae7a7d5f
Daniel Drabik <daniel.drabik94@gmail.com>
2017-10-21 19:39:18 +0200

    Upload

---
+++ Project1/Movie.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Text
+{
+    class Movie
+    {
+        private string title;
+        private string director;
+        private int year;
+            
+        public string Title { get => title; set => title = value; }
+        public string Director { get => director; set => director = value; }
+        public int Year { get => year; set => year = value; }
+
+        public Movie()
+        {
+            Title = "Title";
+            Director = "John Doe";
+            Year = 1900;
+        }
+
+        public void DisplayInformation()
+        {
+            Console.WriteLine("Tytul: {0}", Title);
+            Console.WriteLine("Rezyser: {0}", Director);
+            Console.WriteLine("Rok: {0}", Year);
+            Console.WriteLine("----------------------------");
+        }
+    }
+}
---
+++ Project1/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+
+namespace Text
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Movie[] objMovies = new Movie[1];
+            int choice;
+
+            do
+            {
+                Console.WriteLine("1. Wczytaj dane");
+                Console.WriteLine("2. Wyswietl wszystkie filmy");
+                Console.WriteLine("3. Dodaj film");
+                Console.WriteLine("4. Wyjdz");
+
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        LoadData(ref objMovies);
+                        break;
+                    case 2:
+                        DisplayAllMovies(objMovies);
+                        break;
+                    case 3:
+                        AddMovie(ref objMovies);
+                        break;
+                    default:
+                        break;
+                }
+            } while (choice != 4);
+
+        }
+
+        public static void LoadData(ref Movie[] objMovies)
+        {
+            StreamReader reader = new StreamReader("Movies.txt");
+            int size = Convert.ToInt32(reader.ReadLine());
+            objMovies = new Movie[size];
+
+            for (int index = 0; index < objMovies.Length; index++)
+            {
+                objMovies[index] = new Movie();
+                objMovies[index].Title = reader.ReadLine();
+                objMovies[index].Director = reader.ReadLine();
+                objMovies[index].Year = Convert.ToInt32(reader.ReadLine());
+            }
+
+            reader.Close();
+
+        }
+
+        public static void DisplayAllMovies(Movie[] objMovies)
+        {
+            // Wyswietl wszystkie informacje o przechowywanych filmach korzystając z metody DisplayInformation()   
+            
+        }
+
+        public static void AddMovie(ref Movie[] objMovies)
+        {
+            // Zainicjuj StreamWritera tak aby nadpisywał istniejący plik
+            
+
+            // Zainkrementuj liczbe filmow i wpisz do pliku
+           
+
+            // Stworz nowy obiekt
+            
+
+            // Zbierz informacje od użytkownika
+            
+
+            // Wprowadź nowy film do pliku
+            
+
+            // Wprowadź wszystkie poprzednie filmy
+            
+
+            // Zamknij plik
+           
+
+            // Zaktualizuj tablicę objMovies
+            LoadData(ref objMovies);
+        }
+    }
+}
